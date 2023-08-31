07f4c4ab9ce3b68a650ac86cf654e27fa0d6f7d6
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2023-08-31 22:59:10 +0200

    Redesign card

--- components/LeaderboardCard.tsx
+++ components/LeaderboardCard.tsx
@@ -21,10 +21,10 @@ export default function LeaderboardCard({
 }) {
   return (
     <main
-      className="relative flex h-[512px] w-[1025px] flex-col bg-gradient-to-r from-blue-500  to-cyan-500 text-5xl font-extralight text-white before:absolute before:right-0 before:top-0 before:h-full before:w-[--width] before:bg-neutral-800"
+      className="relative flex h-[384px] w-[1025px] flex-col bg-gradient-to-r from-blue-500  to-cyan-500 text-5xl font-extralight text-white before:absolute before:right-0 before:top-0 before:h-full before:w-[--width] before:bg-neutral-800"
       style={{ "--width": `${100 - 100 * (xp / xpMax)}%` } as CSSProperties}
     >
-      <header className="absolute left-0 top-0 flex w-full items-center justify-between gap-8 bg-black bg-opacity-30 p-4">
+      <header className="z-10 flex items-center justify-between gap-8 bg-neutral-900 p-4">
         <span>#{position}</span>
         <section className="flex min-w-0 grow basis-0 items-center gap-4">
           <Image
@@ -32,20 +32,22 @@ export default function LeaderboardCard({
             width={80}
             height={80}
             alt="Avatar"
-            className="shrink-0 rounded-full"
+            className="rounded-full"
             unoptimized={true}
             priority={true}
             loading={"eager"}
           ></Image>
-          <h1 className="grow overflow-hidden text-ellipsis whitespace-nowrap">
+          <h1 className="overflow-hidden text-ellipsis whitespace-nowrap">
             {user.global_name ?? user.username}
           </h1>
         </section>
         <span className="lowercase">Level {level}</span>
       </header>
-      <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <span className="text-9xl font-bold">{xp}</span>/{xpMax} xp
-      </span>
+      <section className="z-10 flex grow items-center justify-center">
+        <span>
+          <span className="text-9xl font-bold">{xp}</span>/{xpMax} xp
+        </span>
+      </section>
     </main>
   )
 }
